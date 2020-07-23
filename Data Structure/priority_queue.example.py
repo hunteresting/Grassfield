7df1e7bdeceaca91a29a44616609ec346e3ae78d
Queen <bulias@naver.com>
2020-07-23 21:27:00 +0900

    tree

---
+++ Data Structure/priority_queue.example.py
@@ -0,0 +1,13 @@
+# 최대 우선순위 큐
+priority_queue = MaxPriorityQueue()
+
+# 우선순위 큐에 데이터 삽입
+priority_queue.add(5)
+priority_queue.add(2)
+priority_queue.add(7)
+priority_queue.add(8)
+
+print(priority_queue.pop())
+print(priority_queue.pop())
+print(priority_queue.pop())
+print(priority_queue.pop())
